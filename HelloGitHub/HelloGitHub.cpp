d8f4cc4fba52efae4699985b0b5542d2a83ba00d
AngelMueller20 <113391242+AngelMueller20@users.noreply.github.com>
2022-10-10 20:55:07 -0400

    Added a line of code

--- HelloGitHub/HelloGitHub.cpp
+++ HelloGitHub/HelloGitHub.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "Test-Khani\n";
+    std::cout << "Commit-Angel";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
